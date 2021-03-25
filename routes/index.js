441cc7a7ebe75673306a39bfa6d32b6aff4fcdd8
JulianMason <52970488+JulianMason@users.noreply.github.com>
2021-03-25 16:03:15 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require('../config/auth');
-const Story = require('../models/Ticket');
+const Ticket = require('../models/Ticket');
 
 // Landing page
 router.get('/', ensureGuest, (req, res) => res.render('welcome'));
 
 // Dashboard page
-router.get('/dashboard', ensureAuth, (req, res) => 
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const tickets = await Ticket.find({ user: req.user.id })
+    } catch (err) {
+
+    }
+
     res.render('dashboard', {
         name: req.user.name,
         userType: req.user.userType
-    }));
+    })
+})
 
 module.exports = router;
